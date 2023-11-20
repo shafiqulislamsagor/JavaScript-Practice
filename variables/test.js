285092b8cad5da76f29b43360c53d5ff4257038a
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-20 23:28:50 +0600

    JavaScript Variables complete

---
+++ variables/test.js
@@ -0,0 +1,25 @@
+//var/let/const:
+var x = 5;
+var y = 6;
+var z = x + y ;
+
+document.getElementById("a").innerHTML = 
+">>>>> the value of z is : " + z ;
+
+//mixed variables:
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2 ;
+
+document.getElementById("b").innerHTML = 
+">>>>>> the total is : " + total ;
+
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes , I am !'
+
+document.getElementById("c").innerHTML = 
+">>>>>> " + "<br>" + pi + "<br>" + person + "<br>" + answer ;
+
+let carName = "Volvo";
+document.getElementById("d").innerHTML = ">>>>>> " + carName ;

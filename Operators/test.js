a9d5c388c9545b10b0f239bef410c87c55ef9aac
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-21 22:37:58 +0600

    JavaScript adding Operator

---
+++ Operators/test.js
@@ -0,0 +1,8 @@
+let a = 30 ;
+let b = 20 ;
+let c = a + b ;
+document.getElementById("a").innerHTML = c ;
+let d = 20 ;
+let f = 20 ;
+let g = d + f ;
+document.getElementById("b").innerHTML = "20 + 20 = " + g ;

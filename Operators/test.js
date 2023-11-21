54bb7b1c4b9f93b17f0a4d352db975d56ba440ec
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-21 23:12:26 +0600

    JavaScript Operators

--- Operators/test.js
+++ Operators/test.js
@@ -6,3 +6,14 @@ let d = 20 ;
 let f = 20 ;
 let g = d + f ;
 document.getElementById("b").innerHTML = "20 + 20 = " + g ;
+let h = g * 2.5 ;
+document.getElementById("c").innerHTML = "40 x 2.5 =" + h ;
+// letter ar kromo onujayi hoi//
+let text1 = "A";
+let text2 = "C";
+let text3 = "20";
+let text4 = "5";
+let result = text1 < text2;
+let point = text4 > text3 ;
+document.getElementById("d").innerHTML = "A less than B " + result;
+document.getElementById("e").innerHTML = "20 greater than 5 " + point ;
